a31ede311b2f708ce4c24762a41fddc49dfdafd6
Anish Sridhar <anishsridhar@anishs-mbp.lan>
2022-03-10 14:58:55 -0500

    Addition Program Added

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String[] args){
+        int num_1 = 10;
+        int num_2 = 20;
+        System.out.println("Number 1 :"+num1);
+        System.out.println(("Number 2 :"+num2));
+        int result = num1 + num2;
+        System.out.println("Result is : "+result);
+    }
+}
